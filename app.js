1b3d404b48823d49f768a08600ae6bd822105216
c4ctus0x <163348838+c4ctus0x@users.noreply.github.com>
2024-06-26 08:27:16 +0300

    error catching update

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const app = express();
+
 app.use(bodyParser.json());
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.error(err));
+  .catch(err => {
+      console.error(err);
+      process.exit(1);
+  });
+
 app.get('/', (req, res) => {
   res.send('Welcome to the WarehouseTracker API');
 });
-const PORT = process.getenv('PORT') || 3000;
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
