6ccb96fca1751f923473563762300aea7ebb220e
c4ctus0x <163348838+c4ctus0x@users.noreply.github.com>
2024-08-17 14:41:33 +0300

    change variable

--- frontend/App.js
+++ frontend/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import InventoryList from './components/InventoryList';
-import ProductDetails from './components/ProductDetails';
-import NotFound from './components/NotFound';
+import InventoryListView from './components/InventoryList';
+import ProductDetailView from './components/ProductDetails';
+import NotFoundPage from './components/NotFound';
 
-const App = () => {
+const MainAppRouter = () => {
   return (
     <Router>
       <div>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/inventory" component={InventoryList} />
-          <Route path="/product/:productId" component={ProductDetails} />
-          <Route component={NotFound} />
+          <Route path="/inventory" component={InventoryListView} />
+          <Route path="/product/:productId" component={ProductDetailView} />
+          <Route component={NotFoundPage} />
         </Switch>
       </div>
     </Router>
   );
 };
 
-export default App;
+export default MainAppRouter;
